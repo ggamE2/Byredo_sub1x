b7197395db1788f99d6a43e325112905a51bb1c8
ggamE2 <91941591+ggamE2@users.noreply.github.com>
2022-08-07 17:10:51 +0900

    Update common.js

--- common/js/common.js
+++ common/js/common.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
   });
 
   snb.on('click', function(){
-      $(this).parent().addClass('on').children('ul').stop().slideDown(400);
+      $(this).parent().addClass('on').children('ul').slideToggle(400);  
       $(this).parent().siblings('li').removeClass('on').children('ul').stop().slideUp(200);
       return false;
   });
